bfa66b9a5fcfee3b7a4d527ba90f30a3e91421be
X-sensei <77637156+X-sensei@users.noreply.github.com>
2023-08-26 12:54:23 +0530

    Update bgm.js

--- plugins/bgm/bgm.js
+++ plugins/bgm/bgm.js
@@ -1,9 +1,6 @@
 {
 "Ronen" : "https://github.com/X-sensei/Mikasa-plugins/blob/main/plugins/bgm/Ronen.mp3?raw=true",
  "ronen": "https://github.com/X-sensei/Mikasa-plugins/blob/main/plugins/bgm/Ronen.mp3?raw=true",
- "sorry": "https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/Baby%20Im%20So%20Sorry.mp3?raw=true",
- "piano": "https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/Bachelor%20Movie%20Piano%20Bgm%20-%20Adiye%20Bgm.mp3",
-  "first class": "https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/First%20Class%20-%20Arjit%20Singh%20!%20Romantic%20!%20Hindi.mp3",
-  "got you": "https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/I%20Got%20You%20-%20Levitating%20-%20Dua%20Lipa%20!%20English%20Song.mp3",
-  "sad": "https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/alan-faded.mp3"
+ "mikasa": "https://github.com/X-sensei/Mikasa-plugins/blob/main/plugins/bgm/mikasa.mp3?raw=true",
+ "Mikasa": "https://github.com/X-sensei/Mikasa-plugins/blob/main/plugins/bgm/mikasa.mp3?raw=true",
 }

38114dc030089e8947e39876c9ed0dd43fd92394
X-sensei <77637156+X-sensei@users.noreply.github.com>
2023-08-26 12:47:04 +0530

    Update bgm.js

--- plugins/bgm/bgm.js
+++ plugins/bgm/bgm.js
@@ -1,6 +1,6 @@
 {
-"begins" : "https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/And%20so%20it%20begins.mp3?raw=true",
- "sparrow": "https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/Jack%20Sparrow%20Images%20!%20Jack%20Sparrow%20!%20Theme.mp3?raw=true",
+"Ronen" : "https://github.com/X-sensei/Mikasa-plugins/blob/main/plugins/bgm/Ronen.mp3?raw=true",
+ "ronen": "https://github.com/X-sensei/Mikasa-plugins/blob/main/plugins/bgm/Ronen.mp3?raw=true",
  "sorry": "https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/Baby%20Im%20So%20Sorry.mp3?raw=true",
  "piano": "https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/Bachelor%20Movie%20Piano%20Bgm%20-%20Adiye%20Bgm.mp3",
   "first class": "https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/First%20Class%20-%20Arjit%20Singh%20!%20Romantic%20!%20Hindi.mp3",

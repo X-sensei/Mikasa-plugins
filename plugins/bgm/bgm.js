a74cb8846d46f77bd38d6a0995b30fdabab145af
X-sensei <77637156+X-sensei@users.noreply.github.com>
2023-08-26 12:55:02 +0530

    Update bgm.js

--- plugins/bgm/bgm.js
+++ plugins/bgm/bgm.js
@@ -1,6 +1,6 @@
 {
 "Ronen" : "https://github.com/X-sensei/Mikasa-plugins/blob/main/plugins/bgm/Ronen.mp3?raw=true",
  "ronen": "https://github.com/X-sensei/Mikasa-plugins/blob/main/plugins/bgm/Ronen.mp3?raw=true",
- "mikasa": "https://github.com/X-sensei/Mikasa-plugins/blob/main/plugins/bgm/mikasa.mp3?raw=true",
- "Mikasa": "https://github.com/X-sensei/Mikasa-plugins/blob/main/plugins/bgm/mikasa.mp3?raw=true",
+ "mikasa": "https://github.com/X-sensei/Mikasa-plugins/blob/main/plugins/bgm/mention.mp3?raw=true",
+ "Mikasa": "https://github.com/X-sensei/Mikasa-plugins/blob/main/plugins/bgm/mention.mp3?raw=true",
 }
